c84e1d4c0c3d814276ec75f7998727be0a9fdc7a
Patrick Charron-Morneau <patrick.cmorneau@cidco.ca>
2022-08-25 11:16:16 -0400

    more unittest

--- unittest/test_lib.py
+++ unittest/test_lib.py
@@ -29,6 +29,37 @@ class TestLib(unittest.TestCase):
 		ninetyDegreeRotationMatrix = np.array([[math.cos(math.radians(90)),-math.sin(math.radians(90))],[math.sin(math.radians(90)),math.cos(math.radians(90))]])
 		ans2 = ninetyDegreeRotationMatrix.dot(vector)
 		assert np,logical_and(ans,ans2)
+		
+		vector = np.array([5,2])
+		matrix_rotation = mathlib.gen_rotation_matrix(35)
+		ans = matrix_rotation.dot(vector)
+		ninetyDegreeRotationMatrix = np.array([[math.cos(math.radians(35)),-math.sin(math.radians(35))],[math.sin(math.radians(35)),math.cos(math.radians(35))]])
+		ans2 = ninetyDegreeRotationMatrix.dot(vector)
+		assert np,logical_and(ans,ans2)
+		
+		vector = np.array([5,2])
+		matrix_rotation = mathlib.gen_rotation_matrix(180)
+		ans = matrix_rotation.dot(vector)
+		ninetyDegreeRotationMatrix = np.array([[math.cos(math.radians(180)),-math.sin(math.radians(180))],[math.sin(math.radians(180)),math.cos(math.radians(180))]])
+		ans2 = ninetyDegreeRotationMatrix.dot(vector)
+		assert np,logical_and(ans,ans2)
+		
+		vector = np.array([5,2])
+		matrix_rotation = mathlib.gen_rotation_matrix(270)
+		ans = matrix_rotation.dot(vector)
+		ninetyDegreeRotationMatrix = np.array([[math.cos(math.radians(270)),-math.sin(math.radians(270))],[math.sin(math.radians(270)),math.cos(math.radians(270))]])
+		ans2 = ninetyDegreeRotationMatrix.dot(vector)
+		assert np,logical_and(ans,ans2)
+		
+		vector = np.array([5,2])
+		matrix_rotation = mathlib.gen_rotation_matrix(450)
+		ans = matrix_rotation.dot(vector)
+		ninetyDegreeRotationMatrix = np.array([[math.cos(math.radians(450)),-math.sin(math.radians(450))],[math.sin(math.radians(450)),math.cos(math.radians(450))]])
+		ans2 = ninetyDegreeRotationMatrix.dot(vector)
+		assert np,logical_and(ans,ans2)
+		
+		
+		
 
 if __name__ == '__main__':
     unittest.main()
