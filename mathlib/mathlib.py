7b3777752248a84c7c710a28446482620395e7f5
Patrick Charron-Morneau <patrick.cmorneau@cidco.ca>
2022-08-31 15:18:04 -0400

    ok

--- mathlib/mathlib.py
+++ mathlib/mathlib.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import math
-
-#vector = np.array([5,2])
-#print(vector)
+import cv2 as cv
 
 def is_inside_rectangle(rect, point) :
 	if point[0] < rect[0] :
@@ -20,22 +17,7 @@ def gen_rotation_matrix(angle):
 	rotation_matrix = np.array([[math.cos(math.radians(angle)),math.cos(math.radians(angle+90))],[math.sin(math.radians(angle)),math.sin(math.radians(angle+90))]])
 	return rotation_matrix
 
-#matrix_rotation = gen_rotation_matrix(90)
 
-#print(matrix_rotation)
-
-#ans = matrix_rotation.dot(vector)
-
-#print(ans)
-
-"""
-# plotting the graph
-ax = plt.quiver(x_coordinate, y_coordinate,x_direction, y_direction,units='xy', scale=1)
-plt.grid()
-plt.ylim(-10,10)
-plt.xlim(-10,10)
-plt.show()
-"""
 
 def find_min_distance_pts_p(points, point):
 	diff = np.array(points) - np.array(point)
@@ -97,3 +79,17 @@ def angle_between(a, b):
 	if(cos < 0):
 		deg = 180 - deg
 	return deg
+	
+def triangulate(points:list)->list:
+	x = np.array(points)
+	x = x.flatten()
+	topLeft = min(x)
+	bottomRigh = max(x)
+	
+	rect = [topLeft, topLeft, bottomRigh, bottomRigh]
+	subdiv = cv.Subdiv2D(rect)
+	for point in points:
+		subdiv.insert(point)
+	
+	return subdiv.getTriangleList()
+	
